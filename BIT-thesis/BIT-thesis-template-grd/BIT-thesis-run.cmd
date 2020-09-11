1318eeb962f6b83bca3caae3a5c390ff89182c96
Zheng Shuai <iamzs.cn@gmail.com>
2020-09-11 20:30:24 +0800

    整理三个脚本编译文件

--- BIT-thesis/BIT-thesis-template-grd/BIT-thesis-run.cmd
+++ BIT-thesis/BIT-thesis-template-grd/BIT-thesis-run.cmd
@@ -1,9 +1,8 @@
-
 del demo.pdf
+
 xelatex -no-pdf --interaction=nonstopmode demo
 bibtex demo
 xelatex -no-pdf --interaction=nonstopmode demo
 xelatex --interaction=nonstopmode demo
 
 start "" "demo.pdf"
-
